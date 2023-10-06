3cc903e967d5899b55e6b7f1d5fbb1b1b7023b9d
whg <wanghg@skylinematrix.com>
2023-10-06 09:41:47 +0800

    ch08

--- ch08/test_struct.go
+++ ch08/test_struct.go
@@ -27,7 +27,8 @@ func main() {
 	myStruct2.toggle = true
 	fmt.Printf("before %#v\n", myStruct2)
 
-	passValue(myStruct2)
+	//passByValue(myStruct2)
+	passByReference(&myStruct2)
 	fmt.Printf("after %#v\n", myStruct2)
 }
 
@@ -35,10 +36,18 @@ func main() {
 自定义的结构类型在函数参数传递方面的行为与引用类型类似，也是按值传递的，但传递的是结构的副本。
 这意味着函数接收的是结构值的副本，对参数的修改不会影响原始结构的值
 */
-func passValue(myStruct MyStruct) {
-	fmt.Printf("passValue before %#v\n", myStruct)
+func passByValue(myStruct MyStruct) {
+	fmt.Printf("passByValue before %#v\n", myStruct)
 	myStruct.number = 123
 	myStruct.word = "test"
 	myStruct.toggle = false
-	fmt.Printf("passValue after %#v\n", myStruct)
+	fmt.Printf("passByValue after %#v\n", myStruct)
+}
+
+func passByReference(myStruct *MyStruct) {
+	fmt.Printf("passByReference before %#v\n", myStruct)
+	myStruct.number = 123
+	myStruct.word = "test"
+	myStruct.toggle = false
+	fmt.Printf("passByReference after %#v\n", myStruct)
 }
