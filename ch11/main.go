536cc63b338025ca3773196af6c48de3a5a6edc7
whg <wanghg@skylinematrix.com>
2023-10-09 16:25:47 +0800

    ch11

--- ch11/main.go
+++ ch11/main.go
@@ -3,16 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	player := TapeRecorder{}
 	mixtape := []string{"Jessie's Girl", "Whip It", "9 to 5"}
+	var player Player = TapePlayer{} // 此处必须声明为Player接口，不可使用:=短赋值
+	playList(player, mixtape)
+	player = TapeRecorder{}
 	playList(player, mixtape)
 }
 
-func playList(device TapePlayer, songs []string) {
+func playList(device Player, songs []string) {
+	recorder, ok := device.(TapeRecorder) // 类型断言，即接口强转为具体类型
+	if ok {
+		recorder.Start()
+	}
 	for _, song := range songs {
 		device.Play(song)
 	}
 	device.Stop()
+}
+
+type Player interface {
+	Play(song string)
+	Stop()
 }
 
 type TapePlayer struct {
@@ -30,6 +41,9 @@ type TapeRecorder struct {
 	Microphones int
 }
 
+func (t TapeRecorder) Start() {
+	fmt.Println("TapeRecorder Started!")
+}
 func (t TapeRecorder) Record() {
 	fmt.Println("TapeRecorder Recording!")
 }
