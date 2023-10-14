fbec2a87c24f93a4f510e1e45e7fd638a96118e6
whg <wanghg@skylinematrix.com>
2023-10-14 18:31:12 +0800

    ch16

--- ch16/guestbook.go
+++ ch16/guestbook.go
@@ -11,7 +11,9 @@ import (
 
 func main() {
 	http.HandleFunc("/guestbook", guestbook) // 注意匹配url必须/开头
-	err := http.ListenAndServe("localhost:8080", nil)
+	server := &http.Server{Addr: "localhost:8080", Handler: nil}
+	log.Printf("Server[%s] Starting...", server.Addr)
+	err := server.ListenAndServe()
 	//check.CheckAndLog(err)
 	log.Fatal(err) // 不使用上面的CheckAndLog是因为ListenAndServe启动成功会一直阻塞，启动失败会立即返回err
 }
@@ -21,6 +23,12 @@ func guestbook(response http.ResponseWriter, request *http.Request) {
 	fmt.Printf("%#v\n", signatures)
 	html, err := template.ParseFiles("guestbook.html")
 	check.CheckAndLog(err)
-	err = html.Execute(response, nil)
+	err = html.Execute(response,
+		Guestbook{SignatureCount: len(signatures), Signatures: signatures})
 	check.CheckAndLog(err)
+}
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
 }
